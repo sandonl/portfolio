7d20d10b8e763eee36a153f3fae9eef8f23c60b2
Sandon Lai <sandonnlai@gmail.com>
2022-03-06 01:43:44 +1100

    fixed background shadow for nav icons

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -113,12 +113,14 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   }
 `;
 
-// Social Icons
+// Social Icons (Container)
 export const SocialIcons = styled.a`
   transition: 0.3s ease;
   color: white;
   border-radius: 50px;
-  padding: 8px;
+  width: 3.5rem;
+  height: 3.5rem;
+  padding: 5px;
   &:hover {
     background-color: #212d45;
     transform: scale(1.2);
