2b45393cb3543ba6e1122d95ed863da363bb0b23
Sandon Lai <sandonnlai@gmail.com>
2022-03-08 21:59:08 +1100

    added meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,11 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Sandon Lai</title>
+        <meta property="og:title" content="Sandon Lai" key="title" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/sandonl/portfolio/main/readme_image/homepage.png"
+        />
       </Head>
       <Theme>
         <Component {...pageProps} />
